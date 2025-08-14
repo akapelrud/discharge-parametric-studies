91b2cb17c08ab9708cfb2f67f1afb4e7528a42de
André Kapelrud <andre.kapelrud@sintef.no>
2025-08-14 09:07:51 +0200

    flake8 formatting for study definition

--- studies/pressure_study/runs.py
+++ studies/pressure_study/runs.py
@@ -19,9 +19,9 @@ inception_stepper = {
                 "target": "master.inputs",
                 "uri": "DischargeInception.pressure"
                 },
-            "geometry_radius" : {
-                "target" : "master.inputs",
-                "uri" : "Vessel.rod_radius",
+            "geometry_radius": {
+                "target": "master.inputs",
+                "uri": "Vessel.rod_radius",
                 }
             }
         }
@@ -41,41 +41,40 @@ plasma_study_1 = {
             'generic_array_job_jobscript.py'  # used at voltage step level
             ],
         'output_directory': 'study0',
-        'output_dir_prefix':'run_',
+        'output_dir_prefix': 'run_',
         'parameter_space': {
-            "geometry_radius" : {
+            "geometry_radius": {
                 "database": "inception_stepper",  # database dependency
-                "target" : "master.inputs",
-                "uri" : "Vessel.rod_radius",
-                "values" : [ 10e-3 ]
+                "target": "master.inputs",
+                "uri": "Vessel.rod_radius",
+                "values": [1e-3]
                 },
-            "pressure" : {
+            "pressure": {
                 "database": "inception_stepper",  # database dependency
-                "target" : "chemistry.json",
-                "uri" : ["gas", "law", "my_ideal_gas", "pressure"],
-                "values" : [ 1e5 ]  # np.arange(1e5, 11e5, 10e5).tolist()
+                "target": "chemistry.json",
+                "uri": ["gas", "law", "my_ideal_gas", "pressure"],
+                "values": [1e5]  # np.arange(1e5, 11e5, 10e5).tolist()
                 },
-            "photoionization" : {
-                "target" : "chemistry.json",
-                "uri" : [
+            "photoionization": {
+                "target": "chemistry.json",
+                "uri": [
                     "photoionization",
                     [
-                        '+["reaction"=<chem_react>"Y + (O2) -> e + O2+"]', # non-optional match
-                        '*["reaction"=<chem_react>"Y + (O2) -> (null)"]' # optional match (create-if-not-exists)
+                        '+["reaction"=<chem_react>"Y + (O2) -> e + O2+"]',  # non-optional match
+                        '*["reaction"=<chem_react>"Y + (O2) -> (null)"]'  # optional match (create-if-not-exists)
                         ],
                     "efficiency"
                     ],
-                "values" : [[float(v), float(1.0-v)] for v in np.arange(0.0, 1.0, 1.0)]
+                "values": [[float(v), float(1.0-v)] for v in np.arange(0.0, 1.0, 1.0)]
                 },
             }
         }
 
 top_object = dict(
-    databases = [inception_stepper],
-    studies = [plasma_study_1]
-    )
+        databases=[inception_stepper],
+        studies=[plasma_study_1]
+        )
 
 if __name__ == '__main__':
     with open('runs.json', 'w') as f:
         json.dump(top_object, f, indent=4)
-
